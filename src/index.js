f4ceb0818900b2c3f848d8311e09c82675a1427d
t3t3c <65117237+t3t3c@users.noreply.github.com>
2022-08-07 14:39:04 +0200

    website now loads from JS

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { DOMcontent, loadWebsite } from "./pageLoad.js";
+
+console.log("hello world");
+console.log("watch");
+loadWebsite();
---
+++ src/pageLoad.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// const DOMcontent = (() => {
+//   const content = document.querySelector("#content");
+//   const addElement = (element) => {
+//     content.appendChild(document.createElement("div"));
+//   };
+// })();
+
+function loadWebsite() {
+  const content = document.querySelector("#content");
+  const newDiv = document.createElement("h1");
+  newDiv.innerText = "This is the best restaurant!";
+  content.appendChild(newDiv);
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", "../src/img/logo.png");
+  console.log(newImage);
+  content.appendChild(newImage);
+  const newParagraph = document.createElement("p");
+  newParagraph.innerText = "Woah what an amazing taste!";
+  content.appendChild(newParagraph);
+}
+
+export { loadWebsite };
